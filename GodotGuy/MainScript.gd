618a3731702199daef05f1e8b0932e3eea9421c7
SHyguymoll <shymollet@gmail.com>
2023-05-20 18:19:29 -0700

    heavily simplfied things by using Vector2i's

--- GodotGuy/MainScript.gd
+++ GodotGuy/MainScript.gd
@@ -160,117 +160,117 @@ var animations = {
 	"idle":
 		{
 			"animation_length": 1,
-			0: [0,0],
+			0: Vector2i(0,0),
 		},
 	"crouch":
 		{
 			"animation_length": 1,
-			0: [1,0],
+			0: Vector2i(1,0),
 		},
 	"walk_right":
 		{
 			"animation_length": 7,
-			0: [2,0],
-			1: [3,0],
-			2: [4,0],
-			3: [5,0],
-			4: [6,0],
-			5: [7,0],
-			6: [8,0],
-			7: [9,0]
+			0: Vector2i(2,0),
+			1: Vector2i(3,0),
+			2: Vector2i(4,0),
+			3: Vector2i(5,0),
+			4: Vector2i(6,0),
+			5: Vector2i(7,0),
+			6: Vector2i(8,0),
+			7: Vector2i(9,0)
 			
 		},
 	"walk_left":
 		{
 			"animation_length": 7,
-			0: [9,0],
-			1: [8,0],
-			2: [7,0],
-			3: [6,0],
-			4: [5,0],
-			5: [4,0],
-			6: [3,0],
-			7: [2,0]
+			0: Vector2i(9,0),
+			1: Vector2i(8,0),
+			2: Vector2i(7,0),
+			3: Vector2i(6,0),
+			4: Vector2i(5,0),
+			5: Vector2i(4,0),
+			6: Vector2i(3,0),
+			7: Vector2i(2,0)
 		},
 	
 	"stand_a":
 		{
 			"animation_length": 4,
-			0: [6,1],
-			2: [7,1],
-			3: [8,1],
+			0: Vector2i(6,1),
+			2: Vector2i(7,1),
+			3: Vector2i(8,1),
 		},
 	"stand_b":
 		{
 			"animation_length": 6,
-			0: [4,2],
-			2: [5,2],
-			3: [6,2],
-			4: [7,2],
-			5: [8,2],
+			0: Vector2i(4,2),
+			2: Vector2i(5,2),
+			3: Vector2i(6,2),
+			4: Vector2i(7,2),
+			5: Vector2i(8,2),
 		},
 	"stand_c":
 		{
 			"animation_length": 11,
-			0: [0,4],
-			2: [1,4],
-			4: [2,4],
-			5: [3,4],
-			6: [4,4],
-			9: [5,4],
-			10: [6,4],
+			0: Vector2i(0,4),
+			2: Vector2i(1,4),
+			4: Vector2i(2,4),
+			5: Vector2i(3,4),
+			6: Vector2i(4,4),
+			9: Vector2i(5,4),
+			10: Vector2i(6,4),
 		},
 	"crouch_a":
 		{
 			"animation_length": 5,
-			0: [0,1],
-			2: [1,1],
-			3: [2,1],
+			0: Vector2i(0,1),
+			2: Vector2i(1,1),
+			3: Vector2i(2,1),
 		},
 	"crouch_b":
 		{
 			"animation_length": 7,
-			0: [0,2],
-			2: [1,2],
-			4: [2,2],
-			5: [3,2],
+			0: Vector2i(0,2),
+			2: Vector2i(1,2),
+			4: Vector2i(2,2),
+			5: Vector2i(3,2),
 		},
 	"crouch_c":
 		{
 			"animation_length": 10,
-			0: [0,3],
-			1: [1,3],
-			2: [2,3],
-			5: [3,3],
-			6: [4,3],
-			7: [5,3],
-			8: [6,3],
+			0: Vector2i(0,3),
+			1: Vector2i(1,3),
+			2: Vector2i(2,3),
+			5: Vector2i(3,3),
+			6: Vector2i(4,3),
+			7: Vector2i(5,3),
+			8: Vector2i(6,3),
 		},
 	"jump_a": #TODO
 		{
 			"animation_length": 5,
-			0: [0,1],
-			2: [1,1],
-			3: [2,1],
+			0: Vector2i(0,1),
+			2: Vector2i(1,1),
+			3: Vector2i(2,1),
 		},
 	"jump_b": #TODO
 		{
 			"animation_length": 7,
-			0: [0,2],
-			2: [1,2],
-			4: [2,2],
-			5: [3,2],
+			0: Vector2i(0,2),
+			2: Vector2i(1,2),
+			4: Vector2i(2,2),
+			5: Vector2i(3,2),
 		},
 	"jump_c": #TODO
 		{
 			"animation_length": 10,
-			0: [0,3],
-			1: [1,3],
-			2: [2,3],
-			5: [3,3],
-			6: [4,3],
-			7: [5,3],
-			8: [6,3],
+			0: Vector2i(0,3),
+			1: Vector2i(1,3),
+			2: Vector2i(2,3),
+			5: Vector2i(3,3),
+			6: Vector2i(4,3),
+			7: Vector2i(5,3),
+			8: Vector2i(6,3),
 		},
 }
 
@@ -278,11 +278,7 @@ var current_animation: String
 var step_timer = 0
 
 func anim() -> void:
-	if animations[current_animation][step_timer] is Array:
-		$Sprite.frame_coords = Vector2(
-			animations[current_animation][step_timer][0],
-			animations[current_animation][step_timer][1]
-		)
+	$Sprite.frame_coords = animations[current_animation].get(step_timer, $Sprite.frame_coords)
 
 # Hitboxes and Hurtboxes are handled through a dictionary for easy reuse.
 # box format:
