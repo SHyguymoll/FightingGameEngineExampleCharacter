9ed63d4d1396643df7b05fe85b2c3e6d9a841ffd
SHyguymoll <shymollet@gmail.com>
2024-01-17 13:42:33 -0800

    exported hitbox variables, infinite lifetimes

--- GodotGuy/scenes/HitboxScript.gd
+++ GodotGuy/scenes/HitboxScript.gd
@@ -1,17 +1,16 @@
 class_name Hitbox
 extends Area3D
 
-var lifetime : int
-var damage_hit : float
-var damage_block : float
-var stun_hit : int
-var stun_block : int
-var kback_hit : Vector3
-var kback_block : Vector3
-var hit_priority : int
-var type : String
+@export var lifetime : int
+@export var damage_hit : float
+@export var damage_block : float
+@export var stun_hit : int
+@export var stun_block : int
+@export var kback_hit : Vector3
+@export var kback_block : Vector3
+@export var hit_priority : int
+@export var type : String
 
 func _physics_process(_d):
-	lifetime -= 1
-	if lifetime < 0:
-		queue_free()
+	if lifetime > 0: lifetime -= 1
+	if lifetime == -1: queue_free()
