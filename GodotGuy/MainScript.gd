ddd105983fe441a5b9dacf7e20f5d71b26849ddd
SHyguymoll <shymollet@gmail.com>
2024-01-23 20:12:28 -0800

    rotation 3d tracks now also handled
    terrifying quat to euler to quat transformation

--- GodotGuy/MainScript.gd
+++ GodotGuy/MainScript.gd
@@ -151,12 +151,24 @@ var anim_right_suf = "_right"
 func create_mirrored_animation(anim_to_mirror : Animation) -> Animation:
 	var new_anim : Animation = anim_to_mirror.duplicate(true)
 	new_anim.resource_name = anim_to_mirror.resource_name.trim_suffix(anim_right_suf) + anim_left_suf
-	const VECTOR3_FLIP_X := Vector3(-1,1,1)
+	const POSITION_FLIP_X := Vector3(-1,1,1)
+	const ROTATION_FLIP := Vector3(1,1,-1)
 	for track_ind in range((new_anim as Animation).get_track_count()):
-		# only flip the position 3d tracks
+		# only flip the position 3d and rotation 3d tracks
 		if (new_anim as Animation).track_get_type(track_ind) == Animation.TYPE_POSITION_3D:
 			for key_ind in range((new_anim as Animation).track_get_key_count(track_ind)):
-				(new_anim as Animation).track_set_key_value(track_ind, key_ind, ((new_anim as Animation).track_get_key_value(track_ind, key_ind) as Vector3) * VECTOR3_FLIP_X)
+				(new_anim as Animation).track_set_key_value(track_ind, key_ind, ((new_anim as Animation).track_get_key_value(track_ind, key_ind) as Vector3) * POSITION_FLIP_X)
+		if (new_anim as Animation).track_get_type(track_ind) == Animation.TYPE_ROTATION_3D:
+			for key_ind in range((new_anim as Animation).track_get_key_count(track_ind)):
+				(new_anim as Animation).track_set_key_value(
+					track_ind,
+					key_ind,
+					Quaternion.from_euler(
+						(
+							new_anim.track_get_key_value(track_ind, key_ind) as Quaternion
+						).get_euler() * ROTATION_FLIP
+					).normalized()
+				)
 	return new_anim
 
 func initialize_animation_player():
