3babb713eb1547dd3b3fe721a31a9bb2463e69e4
SHyguymoll <shymollet@gmail.com>
2024-01-30 00:18:33 -0800

    added on_hit_sound and on_block_sound variables
    regressed previous _physics_process code back to pre initial sound effect implementation

--- GodotGuy/scenes/HitboxScript.gd
+++ GodotGuy/scenes/HitboxScript.gd
@@ -11,24 +11,17 @@ extends Area3D
 @export_category("Knockback")
 @export var kback_hit : Vector3
 @export var kback_block : Vector3
+@export_category("Audio")
+@export var on_hit_sound : AudioStream
+@export var on_block_sound : AudioStream
 @export_category("Misc")
 @export var hit_priority : int
 @export var hit_type : String
 @export var on_hit : Array
 @export var on_block : Array
 
-var marked := false
-
 func _physics_process(_d):
 	if lifetime > 0:
 		lifetime -= 1
-	if lifetime == 0 or marked:
-		if not get_node_or_null(^"HitSound") and not get_node_or_null(^"BlockSound"):
-			queue_free()
-		else:
-			if not $HitSound.playing and not $BlockSound.playing:
-				queue_free()
-
-func destroy():
-	monitorable = false
-	marked = true
+	if lifetime == 0:
+		queue_free()
