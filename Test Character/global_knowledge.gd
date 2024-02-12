fa138f1327137ea13160d6830adb8c9cf0709688
SHyguymoll <shymollet@gmail.com>
2024-02-11 19:18:04 -0800

    setting up win thresholds

--- Test Character/global_knowledge.gd
+++ Test Character/global_knowledge.gd
@@ -4,6 +4,10 @@ var global_hitstop : int
 var p1_hitstop : int
 var p2_hitstop : int
 
+var win_threshold : int = 2
+var p1_wins : int = 0
+var p2_wins : int = 0
+
 func _physics_process(_d):
 	global_hitstop = max(global_hitstop - 1, 0)
 	p1_hitstop = max(p1_hitstop - 1, 0)
