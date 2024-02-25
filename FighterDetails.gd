d2bd02dc649381ad207ac7dfd4f44f41a4343fda
SHyguymoll <shymollet@gmail.com>
2024-02-24 21:43:08 -0800

    FighterDetails.gd now uses relative paths

--- FighterDetails.gd
+++ FighterDetails.gd
@@ -3,9 +3,10 @@ extends Resource
 ## This script holds the details for the game to read when loading this character.[br]
 ## It should never run under any circumstances.
 
+## the content for the fighter starts from res://Characters/{folder} for cleanliness.
 var folder = "GodotGuy"
 
 var fighter_name = "Godot Guy"
-var tscn_file = "res://GodotGuy/scenes/GodotGuy.tscn"
-var char_select_icon = "res://GodotGuy/icon.svg"
+var fighter_file = "scenes/GodotGuy.tscn"
+var char_select_icon = "IconSmall.png"
 var post_win_quote = "godot👍"
